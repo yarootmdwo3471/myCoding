567e59a28607e53e1983242039fc9169f2cd87c9
yarootmdwo3471 <tmdwo3471@naver.com>
2024-02-19 22:54:09 +0900

    커밋

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,47 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository// <= @component 의 대상이 됨 repository 생성시 필요
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();// 원래는 ConcurrentHashMap<>써야함
+    private static long sequence = 0l;//이것도 AtomicLong 으로 써야함
+
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void delete(Long itemId){
+        Item findItem = findById(itemId);
+        store.remove(findItem);
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
